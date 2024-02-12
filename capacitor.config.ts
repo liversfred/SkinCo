ccf0e1553b3b0af4c86c80bd04f22d8921915b96
lvrsfrd <fmanlusoc@gmail.com>
2024-02-13 00:00:06 +0800

    setup capacitor for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.capstone.skinco',  // domain.identifier.app_name
   appName: 'SkinCo',
   webDir: 'www',
   server: {
